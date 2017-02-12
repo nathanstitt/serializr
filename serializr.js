eb079fbcb460ab1fe8855d3c0401ed30077b2bdb
Nathan Stitt <nathan@stitt.org>
2017-02-11 21:53:23 -0600

    Pass property name and parent object when serializing
    This allows the serializer to make better choices about how or if to
    serialize a property so it can prevent circular references

--- serializr.js
+++ serializr.js
@@ -402,7 +402,7 @@
                     propDef = _defaultPrimitiveProp
                 if (propDef === false)
                     return
-                var jsonValue = propDef.serializer(obj[key])
+                var jsonValue = propDef.serializer(obj[key], key, obj)
                 if (jsonValue === undefined){
                     return
                 }
